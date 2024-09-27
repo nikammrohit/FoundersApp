3755d0b518423a60ac95d783d280169a188fb787
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-27 14:41:05 -0500

    Updated import for App.css in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import './styles/App.css';
 
 function App() {
   return (
