e1e0b4acb5a76c084b9f0f9510ec3fb83917ce0a
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-27 17:14:37 -0500

    Removed the unused variables in the code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,15 @@
 // src/components/Home.js
 // Import React and necessary hooks
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 // Import the fetchUsers function from the API module
-import { fetchUsers } from '../services/api';
+//import { fetchUsers } from '../services/api';
 import LandingPage from './LandingPage';
 import Header from './Header';
 import Footer from './Footer';
 
 // Define the Home component
 const Home = () => {
-    const [users, setUsers] = useState([]);
 
-    // useEffect hook to fetch users when the component mounts
-    useEffect(() => {
-        fetchUsers().then((response) => setUsers(response.data));
-    }, []);
-
-    // Render the list of users
     return (
         <div> 
             <Header />
